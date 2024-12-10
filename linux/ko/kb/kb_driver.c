43023358b6904c53a7765eb045517fefb050fdbe
Noah Netz <noahjnetz@gmail.com>
2024-12-10 14:14:58 -0700

    Keyboard Device Driver

--- linux/ko/kb/kb_driver.c
+++ linux/ko/kb/kb_driver.c
@@ -1,9 +1,8 @@
 /**
- * LED Patterns Platform Device Driver
+ * Keyboard Platform Device Driver
  * 
- * Ryan Dupuis
- * Started: 11/30/2024
- */
+ * Noah Netz
+*/
 
 #include <linux/module.h>
 #include <linux/platform_device.h>
@@ -17,9 +16,8 @@
 
 
 
-#define HPS_LED_CONTROL_OFFSET 0x0
-#define LED_REG_OFFSET 0x4
-#define BASE_PERIOD_OFFSET 0x8
+#define KB_BUFFER_OFFSET 0x0
+#define BYTE_SIZE 16
 
 
 
@@ -30,30 +28,24 @@
  * to be matched with this driver, its device tree node must use the same
  * compatible string as defined here.
  */
-static const struct of_device_id led_patterns_of_match[] =
+static const struct of_device_id keyboard_of_match[] =
 {
-	{.compatible = "dupuis,led_patterns",},
+	{.compatible = "dupuis,keyboard",},
 	{}
 };
 
 /**
- * struct led_patterns_dev - Private led patterns device struct.
- * @base_addr:       Pointer to the component's base address
- * @hps_led_control: Address of the hps_led_control register
- * @base_period:     Address of the base_period register
- * @led_reg:         Pointer to the led_reg register
- * @miscdev:         miscdevice used to create a character device
- * @lock:            mutex used to prevent concurrent writes to memory
+ * struct keyboard_dev - Private keyboard device struct.
+ * @base_addr:        Pointer to the component's base address
+ * @red_duty_cycle:   Address of the kb_buffer register
+ * @lock:             mutex used to prevent concurrent writes to memory
  *
- * An led_patterns_dev struct gets created for each led patterns component.
+ * keyboard_dev struct gets created for each keyboard component.
  */
-struct led_patterns_dev
+struct keyboard_dev
 {
 	void __iomem *base_addr;
-	void __iomem *hps_led_control;
-	void __iomem *base_period;
-	void __iomem *led_reg;
-	struct miscdevice miscdev;
+	void __iomem *kb_buffer;
 	struct mutex lock;
 };
 
@@ -62,358 +54,74 @@ struct led_patterns_dev
 // ATTRIBUTES -----------------------------------------------------------------
 
 /**
- * led_reg_show() - Return the led_reg value to userspace via sysfs.
- * @dev:  Device structure for the led_patterns component. This is embedded
- *        in the led_patterns' platform device struct.
+ * kb_buffer_show() - Return the kb_buffer value to userspace via sysfs.
+ * @dev:  Device structure for the keyboard component. This is embedded
+ *        in the keyboard's platform device struct.
  * @attr: Unused.
  * @buf:  Buffer that gets returned to userspace.
  * 
  * Return: The number of bytes read.
  */
-static ssize_t led_reg_show(struct device *dev,
+static ssize_t kb_buffer_show(struct device *dev,
 	struct device_attribute *attr, char *buf)
 {
-	u8 led_reg;
-	struct led_patterns_dev *priv = dev_get_drvdata(dev);
+	unsigned int kb_buffer;
+	struct keyboard_dev *priv = dev_get_drvdata(dev);
 	
-	led_reg = ioread32(priv->led_reg);
+	kb_buffer = ioread32(priv->kb_buffer);
 	
-	return scnprintf(buf, PAGE_SIZE, "%u\n", led_reg);
+	return scnprintf(buf, PAGE_SIZE, "%u\n", kb_buffer);
 }
 
 /**
- * led_reg_store() - Store the led_reg value.
- * @dev:  Device structure for the led_patterns component. This is embedded
- *        in the led_patterns' platform device struct.
- * @attr: Unused.
- * @buf:  Buffer that contains the led_reg value being written.
- * @size: The number of butes being written.
- * 
- * Return: The number of bytes stored.
- */
-static ssize_t led_reg_store(struct device *dev,
+ * kb_buffer_store() - Attempt to store the kb_buffer value.
+ */ 
+static ssize_t kb_buffer_store(struct device *dev,
 	struct device_attribute *attr, const char *buf, size_t size)
 {
-	u8 led_reg;
-	int ret;
-	struct led_patterns_dev *priv = dev_get_drvdata(dev);
-	
-	// Parse the string we received as a u8
-	// See https://elixir.bootlin.com/linux/latest/source/lib/kstrtox.c#L289
-	ret = kstrtou8(buf, 0, &led_reg);
-	if (ret < 0)
-	{
-		return ret;
-	}
-	
-	iowrite32(led_reg, priv->led_reg);
-	
-	// Write was successful, so we return the number of bytes we wrote.
-	return size;
-}
-
-
-
-/**
- * hps_led_control_show() - Return the hps_led_control value to userspace
- *                          via sysfs.
- * @dev:  Device structure for the led_patterns component. This is embedded
- *        in the led_patterns' platform device struct.
- * @attr: Unused.
- * @buf:  Buffer that gets returned to userspace.
- * 
- * Return: The number of bytes read.
- */
-static ssize_t hps_led_control_show(struct device *dev,
-	struct device_attribute *attr, char *buf)
-{
-	bool hps_control;
-	
-	// Get the private led_patterns data out of the dev struct
-	struct led_patterns_dev *priv = dev_get_drvdata(dev);
-	
-	hps_control = ioread32(priv->hps_led_control);
-	
-	return scnprintf(buf, PAGE_SIZE, "%u\n", hps_control);
-}
-
-/**
- * hps_led_control_store() - Store the hps_led_control value.
- * @dev:  Device structure for the led_patterns component. This is embedded
- *        in the led_patterns' platform device struct.
- * @attr: Unused.
- * @buf:  Buffer that contains the hps_led_control value being written.
- * @size: The number of bytes being written.
- * 
- * Return: The number of bytes stored.
- */
-static ssize_t hps_led_control_store(struct device *dev,
-	struct device_attribute *attr, const char *buf, size_t size)
-{
-	bool hps_control;
-	int ret;
-	struct led_patterns_dev *priv = dev_get_drvdata(dev);
-	
-	// Parse the string we received as a bool
-	// See https://elixir.bootlin.com/linux/latest/source/lib/kstrtox.c#L289
-	ret = kstrtobool(buf, &hps_control);
-	if (ret < 0)
-	{
-		// kstrtobool returned an error
-		return ret;
-	}
-	
-	iowrite32(hps_control, priv->hps_led_control);
-	
-	// Write was successful, so we return the number of bytes we wrote.
-	return size;
-}
-
-
-
-/**
- * base_period_show() - Return the base_period value to userpsace via sysfs.
- * @dev:  Device structure for the led_patterns component. This is embedded
- *        in the led_patterns' platform device struct.
- * @attr: Unused.
- * @buf:  Buffer that gets returned to userspace.
- * 
- * Return: The number of bytes read.
- */
-static ssize_t base_period_show(struct device *dev,
-	struct device_attribute *attr, char *buf)
-{
-	u8 base_period;
-	struct led_patterns_dev *priv = dev_get_drvdata(dev);
-	
-	base_period = ioread32(priv->base_period);
-	
-	return scnprintf(buf, PAGE_SIZE, "%u\n", base_period);
-}
-
-/**
- * base_period_store() - Store the base_period value.
- * @dev:  Device structure for the led_patterns component. This is embedded
- *        in the led_patterns' platform device struct.
- * @attr: Unused.
- * @buf:  Buffer that contains the base_period value being written.
- * @size: The number of bytes being written.
- * 
- * Return: The number of bytes stored.
- */
-static ssize_t base_period_store(struct device *dev,
-	struct device_attribute *attr, const char *buf, size_t size)
-{
-	u8 base_period;
-	int ret;
-	struct led_patterns_dev *priv = dev_get_drvdata(dev);
-	
-	// Parse the string we received as a u8
-	// See https://elixir.bootlin.com/linux/latest/source/lib/kstrtox.c#L289
-	ret = kstrtou8(buf, 0, &base_period);
-	if (ret < 0)
-	{
-		// kstrtou8 returned an error
-		return ret;
-	}
-	
-	iowrite32(base_period, priv->base_period);
-	
-	// Write was successful, so we return the number of bytes we wrote.
+	pr_err("Keyboard buffer cannot be written to.");
 	return size;
 }
 
 
 
 // Define sysfs attributes
-static DEVICE_ATTR_RW(hps_led_control);
-static DEVICE_ATTR_RW(led_reg);
-static DEVICE_ATTR_RW(base_period);
+static DEVICE_ATTR_RW(kb_buffer);
 
 // Create an attribute group so the device core can export attributes for us
-static struct attribute *led_patterns_attrs[] =
+static struct attribute *keyboard_attrs[] =
 {
-	&dev_attr_hps_led_control.attr,
-	&dev_attr_base_period.attr,
-	&dev_attr_led_reg.attr,
+	&dev_attr_kb_buffer.attr,
 	NULL,
 };
-ATTRIBUTE_GROUPS(led_patterns);
+ATTRIBUTE_GROUPS(keyboard);
 
 // END OF ATTRIBUTES ----------------------------------------------------------
-
-
-
-// FILE OPERATIONS ------------------------------------------------------------
-
-/**
- * led_patterns_read() - Read method for the led_patterns char device
- * @file:   Pointer to the char device file struct.
- * @buf:    User-space buffer to read the value into.
- * @count:  The number of bytes being requested.
- * @offset: The byte offset in the file being read from.
- *
- * Return: On success, the number of bytes written is returned and the
- * offset @offset is advanced by this number. On error, a negative error
- * value is returned.
- */
-static ssize_t led_patterns_read(struct file *file, char __user *buf,
-	size_t count, loff_t *offset)
-{
-	u32 val;
-	
-	/*
-	 * Get the device's private data from the file struct's private_data
-	 * field. The private_data field is equal to the miscdev field in the
-	 * led_patterns_dev struct. container_of returns the
-	 * led_patterns_dev struct that contains the miscdev in private_data.
-	 */
-	struct led_patterns_dev *priv = container_of(file->private_data,
-		struct led_patterns_dev, miscdev);
-	
-	// Check file offset to make sure we are reading from a valid location.
-	if (*offset < 0)
-	{
-		// We can't read from a negative file position.
-		return -EINVAL;
-	}
-	if (*offset >= 16)
-	{
-		// We can't read from a position past the end of our device.
-		return 0;
-	}
-	if ((*offset % 0x4) != 0)
-	{
-		// Prevent unaligned access.
-		pr_warn("led_patterns_read: unaligned access\n");
-		return -EFAULT;
-	}
-	
-	val = ioread32(priv->base_addr + *offset);
-	
-	// Copy the value to userspace.
-	size_t ret = copy_to_user(buf, &val, sizeof(val));
-	if (ret == sizeof(val))
-	{
-		pr_warn("led_patterns_read: nothing copied\n");
-		return -EFAULT;
-	}
-	
-	// Increment the file offset by the number of bytes we read.
-	*offset = *offset + sizeof(val);
-	
-	return sizeof(val);
-}
-
-
-
-/**
- * led_patterns_write() - Write method for the led_patterns char device
- * @file:   Pointer to the char device file struct.
- * @buf:    User-space buffer to read the value from.
- * @count:  The number of bytes being written.
- * @offset: The byte offset in the file being written to.
- *
- * Return: On success, the number of bytes written is returned and the
- * offset @offset is advanced by this number. On error, a negative error
- * value is returned.
- */
-static ssize_t led_patterns_write(struct file *file, const char __user *buf,
-	size_t count, loff_t *offset)
-{
-	u32 val;
-	
-	struct led_patterns_dev *priv = container_of(file->private_data,
-		struct led_patterns_dev, miscdev);
-	
-	if (*offset < 0)
-	{
-		return -EINVAL;
-	}
-	
-	if (*offset >= 16)
-	{
-		return 0;
-	}
-	
-	if ((*offset % 0x4) != 0)
-	{
-		pr_warn("led_patterns_write: unaligned access\n");
-		return -EFAULT;
-	}
-	
-	mutex_lock(&priv->lock);
-	
-	// Get the value from userspace.
-	size_t ret = copy_from_user(&val, buf, sizeof(val));
-	if (ret != sizeof(val))
-	{
-		iowrite32(val, priv->base_addr + *offset);
-		
-		// Increment the file offset by the number of bytes we wrote.
-		*offset = *offset + sizeof(val);
-		
-		// Return the number of bytes we wrote.
-		ret = sizeof(val);
-	}
-	else
-	{
-		pr_warn("led_patterns_write: nothing copied from user space\n");
-		ret = -EFAULT;
-	}
-	
-	mutex_unlock(&priv->lock);
-	
-	return ret;
-}
-
-
-
-/**
- * led_patterns_fops - File operations supported by the led_patterns driver
- * @owner:  The led_patterns driver owns the file operations; this ensures
- *          that the driver can't be removed while the character device is
- *          still in use.
- * @read:   The read function.
- * @write:  The write function.
- * @llseek: We use the kernel's default_llseek() function; this allows users
- *          to change what position they are writing/reading to/from.
- */
-static const struct file_operations led_patterns_fops =
-{
-	.owner = THIS_MODULE,
-	.read = led_patterns_read,
-	.write = led_patterns_write,
-	.llseek = default_llseek,
-};
-
-// END OF FILE OPERATIONS -----------------------------------------------------
 
 
 
 // PROBE AND REMOVE -----------------------------------------------------------
 
 /**
- * led_patterns_probe() - Initialize led patterns device when a match is found.
- * @pdev: Platform device structure associated with led patterns device;
+ * keyboard_probe() - Initialize keyboard device when a match is found.
+ * @pdev: Platform device structure associated with keyboard device;
  *        pdev is automatically created by the driver core based upon
  *        the device tree node.
  *
- * It's called by the kernel when an led_patterns device is found in the device
- * tree.
+ * It's called by the kernel when a keyboard device is found in the device tree.
  */
-static int led_patterns_probe(struct platform_device *pdev)
+static int keyboard_probe(struct platform_device *pdev)
 {
-	pr_info("led_patterns_probe\n");
+	pr_info("keyboard_probe\n");
 	
 	/**
-	 * Allocate kernel memory for the led patterns device and set it to 0.
+	 * Allocate kernel memory for the pwm device and set it to 0.
 	 * GFP_KERNEL specifies that we are allocating normal kernel RAM;
 	 * see the kmalloc documentation for more info. The allocated memory
 	 * is automatically freed when the device is removed.
 	 */
-	struct led_patterns_dev *priv;
-	priv = devm_kzalloc(&pdev->dev, sizeof(struct led_patterns_dev),
+	struct keyboard_dev *priv;
+	priv = devm_kzalloc(&pdev->dev, sizeof(struct keyboard_dev),
 		GFP_KERNEL);
 	if (!priv)
 	{
@@ -435,60 +143,29 @@ static int led_patterns_probe(struct platform_device *pdev)
 	}
 	
 	// Set the memory addresses for each register.
-	priv->hps_led_control = priv->base_addr + HPS_LED_CONTROL_OFFSET;
-	priv->base_period = priv->base_addr + BASE_PERIOD_OFFSET;
-	priv->led_reg = priv->base_addr + LED_REG_OFFSET;
-	
-	// Enable software-control mode and turn all the LEDs on, just for fun.
-	iowrite32(0x00000001, priv->hps_led_control);
-	iowrite32(0x000000FF, priv->led_reg);
-	
-	// Initialize the misc device parameters
-	priv->miscdev.minor = MISC_DYNAMIC_MINOR;
-	priv->miscdev.name = "led_patterns";
-	priv->miscdev.fops = &led_patterns_fops;
-	priv->miscdev.parent = &pdev->dev;
-	
-	// Register the misc device; this creates a char dev at /dev/led_patterns
-	size_t ret = misc_register(&priv->miscdev);
-	if (ret)
-	{
-		pr_err("Failed to register misc device");
-		return ret;
-	}
+	priv->kb_buffer = priv->base_addr + KB_BUFFER_OFFSET;
 	
 	/**
-	 * Attach the led patterns's private data to the platform device's struct.
+	 * Attach the pwm's private data to the platform device's struct.
 	 * This is so we can access our state container in the other functions.
 	 */
 	platform_set_drvdata(pdev, priv);
 	
-	pr_info("led_patterns_probe successful.\n");
+	pr_info("keyboard_probe successful! :)\n");
 	return 0;
 }
 
 
 
 /**
- * led_patterns_remove() - Remove an led patterns device.
- * @pdev: Platform device structure associated with our led patterns device.
+ * keyboard_remove() - Remove a keyboard device.
+ * @pdev: Platform device structure associated with our keyboard device.
  * 
- * It's called when an led_patterns device is removed or the driver is removed.
+ * It's called when an keyboard device is removed or the driver is removed.
  */
-static int led_patterns_remove(struct platform_device *pdev)
-{
-	pr_info("led_patterns_remove\n");
-	
-	// Get the led patterns's private data from the platform device.
-	struct led_patterns_dev *priv = platform_get_drvdata(pdev);
-	
-	// Disable software-control mode, just for kicks.
-	iowrite32(0, priv->hps_led_control);
-	
-	// Deregister the misc device and remove the /dev/led_patterns file.
-	misc_deregister(&priv->miscdev);
-	
-	pr_info("led_patterns_remove successful\n");
+static int keyboard_remove(struct platform_device *pdev)
+{	
+	pr_info("keyboard_remove successful! :)\n");
 	return 0;
 }
 
@@ -497,29 +174,29 @@ static int led_patterns_remove(struct platform_device *pdev)
 
 
 /**
- * struct led_patterns_driver - Platform driver struct for this driver
+ * struct keyboard_driver - Platform driver struct for this driver
  * @probe:                 Pointer to function called when device is found
  * @remove:                Pointer to function called when device is removed
  * @driver.owner:          Which module owns this driver
  * @driver.name:           Name of driver
  * @driver.of_match_table: Device tree match table
  */
-static struct platform_driver led_patterns_driver = {
-	.probe = led_patterns_probe,
-	.remove = led_patterns_remove,
+static struct platform_driver keyboard_driver = {
+	.probe = keyboard_probe,
+	.remove = keyboard_remove,
 	.driver = {
 		.owner = THIS_MODULE,
-		.name = "led_patterns",
-		.of_match_table = led_patterns_of_match,
-		.dev_groups = led_patterns_groups,
+		.name = "keyboard",
+		.of_match_table = keyboard_of_match,
+		.dev_groups = keyboard_groups,
 	},
 };
 
 
 
-module_platform_driver(led_patterns_driver);
+module_platform_driver(keyboard_driver);
 
-MODULE_DEVICE_TABLE(of, led_patterns_of_match);
+MODULE_DEVICE_TABLE(of, keyboard_of_match);
 MODULE_LICENSE("Dual MIT/GPL");
-MODULE_AUTHOR("Ryan Dupuis");
-MODULE_DESCRIPTION("led_patterns driver");
+MODULE_AUTHOR("Noah Netz");
+MODULE_DESCRIPTION("keyboard driver");
